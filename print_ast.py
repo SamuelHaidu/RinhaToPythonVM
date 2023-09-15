b3fbfcb3ab923f2277bc268581c98b4805bec4c0
Samuel Haidu <samuelhaidu3@gmail.com>
2023-09-14 21:45:33 -0300

    Better AST pretty print

--- print_ast.py
+++ print_ast.py
@@ -1,5 +1,4 @@
 import json
-
 from compiler.ast.rinha_ast import parse_json_to_object
 from compiler.ast.ast_objects import (
     Var,
@@ -20,63 +19,70 @@ from compiler.ast.ast_objects import (
 
 
 def print_tree(node, depth=0):
-    ident = "  " * depth
+    indent = "  " * depth
+    prefix = "│   " * (depth - 1) + "├─ " if depth > 0 else ""
+
+    def format_node(node):
+        if isinstance(node, File):
+            return f"[File {node.name}]"
+        elif isinstance(node, Var):
+            return f"Var {node.text}"
+        elif isinstance(node, Function):
+            params = " ".join(param.text for param in node.parameters)
+            return f"Function {params}"
+        elif isinstance(node, Call):
+            return "Call"
+        elif isinstance(node, Let):
+            return f"Let {node.name.text}"
+        elif isinstance(node, Str):
+            return f'String "{node.value}"'
+        elif isinstance(node, Int):
+            return f"Int {node.value}"
+        elif isinstance(node, Binary):
+            return f"Binary Operator {node.op.value}"
+        elif isinstance(node, Bool):
+            return f"Bool {node.value}"
+        elif isinstance(node, If):
+            return "If"
+        elif isinstance(node, Tuple):
+            return "Tuple"
+        elif isinstance(node, First):
+            return "First"
+        elif isinstance(node, Second):
+            return "Second"
+        elif isinstance(node, Print):
+            return "Print"
+
+    print(f"{prefix}{format_node(node)}")
+
     if isinstance(node, File):
-        print("File: " + node.name)
         print_tree(node.expression, depth + 1)
-    elif isinstance(node, Var):
-        print(ident + "Var " + node.text)
     elif isinstance(node, Function):
-        print(ident + "Function", end=" ")
-        for param in node.parameters:
-            print(param.text, end=" ")
-        print()
         print_tree(node.value, depth + 1)
     elif isinstance(node, Call):
-        print(ident + "Call:")
         print_tree(node.callee, depth + 1)
         for arg in node.arguments:
-            print_tree(arg, depth + 1)
+            print_tree(arg, depth + 2)
     elif isinstance(node, Let):
-        print(ident + "Let:")
-        print(ident + "Name: " + node.name.text)
         print_tree(node.value, depth + 1)
         print_tree(node.next_term, depth)
-    elif isinstance(node, Str):
-        print(ident + 'String: "' + node.value + '"')
-    elif isinstance(node, Int):
-        print(ident + "Int: " + str(node.value))
     elif isinstance(node, Binary):
-        print(ident + "Binary:")
         print_tree(node.lhs, depth + 1)
-        print(ident + "Operator: " + node.op.value)
         print_tree(node.rhs, depth + 1)
-    elif isinstance(node, Bool):
-        print(ident + "Bool: " + str(node.value))
     elif isinstance(node, If):
-        print(ident + "If:")
         print_tree(node.condition, depth + 1)
-        print(ident + "Then:")
         print_tree(node.then, depth + 1)
-        print(ident + "Else:")
+        print(prefix + "otherwise")
         print_tree(node.otherwise, depth + 1)
     elif isinstance(node, Tuple):
-        print(ident + "Tuple:")
         print_tree(node.first, depth + 1)
         print_tree(node.second, depth + 1)
-    elif isinstance(node, First):
-        print(ident + "First:")
-        print_tree(node.value, depth + 1)
-    elif isinstance(node, Second):
-        print(ident + "Second:")
-        print_tree(node.value, depth + 1)
-    elif isinstance(node, Print):
-        print(ident + "Print:")
+    elif isinstance(node, First) or isinstance(node, Second) or isinstance(node, Print):
         print_tree(node.value, depth + 1)
 
 
 if __name__ == "__main__":
-    with open("./tuple_test/tuple_test.json", "r") as f:
+    with open("./test_if_else/test_if_else.json", "r") as f:
         tree_json = json.load(f)
         tree = parse_json_to_object(tree_json)
         print_tree(tree)
